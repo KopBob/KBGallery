215759cdae2fc63cbe21488f04d2c4d508c8d8d0
Boris Kopin <kopbob@gmail.com>
2012-09-05 20:20:39 +0400

    Initial commit!

---
+++ KBGallery/KBGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KBGallery
+//
+//  Created by Boris kopin on 05.09.12.
+//  Copyright (c) 2012 Boris Kopin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBAppDelegate class]));
+    }
+}
